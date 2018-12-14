002ef7ee72646740c3f4546dd186967d9ab0c83a
YasinMahdizadeh <yasin.mahdizade@gmail.com>
2018-12-14 15:43:42 +0330

    I've added my first file

---
+++ Heiiibot.py
@@ -0,0 +1,12 @@
+import requests
+
+bot_username = 'heiiibot'
+bot_token = '549865845:AAHyaPcpbXy8SG8hoyDCzNucTKRjsaNZa5I'
+bot_url='https://api.telegram.org/bot%s/'%(bot_token)
+
+def get_url(url):
+    pasokh = requests.get(url)
+    content = pasokh.content.decode("utf8")
+    return content
+
+print(get_url(bot_url))

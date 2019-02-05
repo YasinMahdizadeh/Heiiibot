3cd4cb17c725089de267e869e9ba9770122e207b
YasinMahdizadeh <yasin.mahdizade@gmail.com>
2019-02-05 20:33:54 +0330

    some line added and now this bot can run along the time and update his information every 0.5 seconds.

--- Heiiibot.py
+++ Heiiibot.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 #We can use https://api.telegram.org/bot<token>/getme to get Information about our bot_token
 
 #The simplest way for us to retrieve
@@ -42,11 +43,25 @@ def get_last_chat_id_and_text(updates):
 def send_message(text, chat_id):
     url = bot_url + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
+def main():
+    last_textchat = (None,None)
+    while True:
+        text, chat = get_last_chat_id_and_text(get_updates())
+        if ( (text,chat) != last_textchat ):
+            send_message(text, chat)
+            last_textchat = (text, chat)
+        time.sleep(0.5)
 
+if __name__ == '__main__':
+    main()
+    
+'''
 print(get_updates())
 text, chat = get_last_chat_id_and_text(get_updates())
 send_message(text ,chat)
 updates = get_updates()
+'''
+
 
 #print ( updates["result"][0]["message"]["text"])
 #and it's how it works...
